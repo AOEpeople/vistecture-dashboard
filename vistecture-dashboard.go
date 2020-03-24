a099f484e44d11a1f59278a71c206bcf4bb041d3
Bastian Ike <bastian.ike@aoe.com>
2020-03-24 16:44:04 +0100

    cleanup and release v2.1.0

--- vistecture-dashboard.go
+++ vistecture-dashboard.go
@@ -12,7 +12,7 @@ func main() {
 	flag.Set("alsologtostderr", "true")
 
 	d := &interfaces.DashboardController{}
-	flag.StringVar(&d.ProjectPath, "config", "example", "Path to project config")
+	flag.StringVar(&d.ProjectPath, "config", "example/project.yml", "Path to project config")
 	flag.StringVar(&d.Templates, "Templates", "templates/dashboard", "Path to dashboard.html and static/ folder")
 	flag.StringVar(&d.Listen, "Listen", ":8080", "server Listen address")
 	flag.BoolVar(&d.DemoMode, "Demo", false, "Demo mode (for templating, demo)")
