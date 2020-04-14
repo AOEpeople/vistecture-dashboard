4e3f6049c7166d6441a1d6f264a23052418ea402
jochen weber <jochen.weber@aoe.com>
2020-04-14 17:36:26 +0200

    add basic test for checkHealth

---
+++ src/model/kube/statusFetcher_test.go
@@ -0,0 +1,30 @@
+package kube
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCheckHealth_AllHealty(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("{\"services\": []}"))
+
+	}))
+
+	healthStatusOfService, reason, _ := checkHealth(AppDeploymentInfo{}, server.URL, "/")
+	if !healthStatusOfService {
+		t.Errorf("healthStatusOfService should be true %v", reason)
+	}
+}
+
+func TestCheckHealth_UnhealthyService(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("{\"services\": [{\"name\": \"dummy\", \"alive\": false, \"details\": \"dummy\"}]}"))
+	}))
+
+	healthStatusOfService, _, _ := checkHealth(AppDeploymentInfo{}, server.URL, "/nonexistingpath")
+	if healthStatusOfService {
+		t.Errorf("healthStatusOfService should be false")
+	}
+}
